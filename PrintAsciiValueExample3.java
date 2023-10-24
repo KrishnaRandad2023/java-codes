96b5f5f088f9e4e902e3eacb4b319ebb83f8f0e4
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:29:12 +0530

    Create PrintAsciiValueExample3.java

---
+++ PrintAsciiValueExample3.java
@@ -0,0 +1,14 @@
+1.	public class PrintAsciiValueExample3   
+2.	{  
+3.	public static void main(String[] args)   
+4.	{  
+5.	//characters whose ASCII value to be found  
+6.	char ch1 = 'a';  
+7.	char ch2 = 'b';  
+8.	//casting or converting a charter into int type  
+9.	int ascii1 = (int) ch1;  
+10.	int ascii2 = (int) ch2;  
+11.	System.out.println("The ASCII value of " + ch1 + " is: " + ascii1);  
+12.	System.out.println("The ASCII value of " + ch1 + " is: " + ascii2);  
+13.	}  
+14.	}  
