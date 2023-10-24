71f7e79fe46d8cc62d14d331b7e4cb958beea1b2
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:02:36 +0530

    Create FactorialExample.java

---
+++ FactorialExample.java
@@ -0,0 +1,10 @@
+1.	class FactorialExample{  
+2.	 public static void main(String args[]){  
+3.	  int i,fact=1;  
+4.	  int number=5;//It is the number to calculate factorial    
+5.	  for(i=1;i<=number;i++){    
+6.	      fact=fact*i;    
+7.	  }    
+8.	  System.out.println("Factorial of "+number+" is: "+fact);    
+9.	 }  
+10.	} 
