920242e2e25214a76c5f8e26077b9fa614e146a6
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:28:24 +0530

    Create PrintAsciiValueExample1.java

---
+++ PrintAsciiValueExample1.java
@@ -0,0 +1,14 @@
+1.	public class PrintAsciiValueExample1   
+2.	{  
+3.	public static void main(String[] args)   
+4.	{  
+5.	// character whose ASCII value to be found  
+6.	char ch1 = 'a';  
+7.	char ch2 = 'b';  
+8.	// variable that stores the integer value of the character  
+9.	int asciivalue1 = ch1;  
+10.	int asciivalue2 = ch2;  
+11.	System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+12.	System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+13.	}  
+14.	}  
