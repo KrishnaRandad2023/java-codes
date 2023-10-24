8e93cd80b4e69d30a0ac956ff08fab057a0b8491
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:11:04 +0530

    Create DownTrianglePattern.java

---
+++ DownTrianglePattern.java
@@ -0,0 +1,27 @@
+1.	import java.util.Scanner;  
+2.	public class DownTrianglePattern  
+3.	{  
+4.	public static void main(String[] args)  
+5.	{  
+6.	int i, j, k, rows=9;     
+7.	for (i=rows; i>= 1 ; i--)  
+8.	{  
+9.	for (j = i; j<rows ; j++)   
+10.	{  
+11.	System.out.print(" ");  
+12.	}     
+13.	for (k = 1; k <= (2*i -1) ;k++)   
+14.	{  
+15.	if( k==1 || i == rows || k==(2*i-1))   
+16.	{  
+17.	System.out.print("*");  
+18.	}  
+19.	else   
+20.	{  
+21.	System.out.print(" ");  
+22.	}  
+23.	}  
+24.	System.out.println("");  
+25.	}  
+26.	}  
+27.	}  
