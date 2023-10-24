9d952c717678a1b959c45f1e6e28a05a95499d58
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:28:45 +0530

    Create PrintAsciiValueExample2.java

---
+++ PrintAsciiValueExample2.java
@@ -0,0 +1,10 @@
+1.	public class PrintAsciiValueExample2  
+2.	{  
+3.	public static void main(String[] String)   
+4.	{  
+5.	int ch1 = 'a';  
+6.	int ch2 = 'b';  
+7.	System.out.println("The ASCII value of a is: "+ch1);  
+8.	System.out.println("The ASCII value of b is: "+ch2);  
+9.	}  
+10.	}  
